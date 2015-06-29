2d1b522c5c59caaec4366819cd748847057da47e
Nick Mow <nickmow@gmail.com>
2015-06-29 08:08:41 +0000

    Removed sandbox test

--- tests/SmoovieTest.php
+++ tests/SmoovieTest.php
@@ -3,12 +3,6 @@
 use NM\Smoovie\Smoovie;
 
 class SmoovieTest extends PHPUnit_Framework_TestCase {
-
-    public function testNachoHasCheese()
-    {
-        $nacho = new Smoovie;
-        $this->assertTrue($nacho->hasCheese());
-
-    }
+    
 
 }
