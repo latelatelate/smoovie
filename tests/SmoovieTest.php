f16de7579365b54609e38f81aa17cae4e71c1afc
Nick Mow <nickmow@gmail.com>
2015-07-03 04:34:01 +0000

    Added FileNotReadableException test

--- tests/SmoovieTest.php
+++ tests/SmoovieTest.php
@@ -2,7 +2,20 @@
 
 use NM\Smoovie\Smoovie;
 
-class SmoovieTest extends PHPUnit_Framework_TestCase {
-    
+class SmoovieTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @expectedException Exception
+     */
+    public function testFileNotReadableException() {
+        $file = '/_files/6504/58/6332_unreadable.xlsx';
+        $s = new Smoovie();
+
+        $s->make($file);
+
+        $this->assertException($s->make($file));
+        
+    }
 
 }
