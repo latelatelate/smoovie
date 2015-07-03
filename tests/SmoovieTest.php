67210b261deca16e16f6b4a16abd4f6e9b85267d
Nick Mow <nickmow@gmail.com>
2015-07-03 05:27:43 +0000

    Added tests for make(), duration(), frames(), fps()

--- tests/SmoovieTest.php
+++ tests/SmoovieTest.php
@@ -11,11 +11,57 @@ class SmoovieTest extends PHPUnit_Framework_TestCase
     public function testFileNotReadableException() {
         $file = '/_files/6504/58/6332_unreadable.xlsx';
         $s = new Smoovie();
+        $s->make($file);
+    }
 
+    /**
+     * @expectedException Exception
+     */
+    public function testFileTypeNotAllowedException() {
+        $file = __DIR__.'/dummy/test.csv';
+        $s = new Smoovie();
+        $s->make($file);
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testDurationCalledWithoutSourceFileException() {
+        $s = new Smoovie();
+        $s->duration();
+    }
+
+    public function testDurationIsFloat() {
+        $file = __DIR__.'/dummy/test.mp4';
+        $s = new Smoovie();
         $s->make($file);
 
-        $this->assertException($s->make($file));
-        
+        $duration = $s->duration();
+
+        $this->assertInternalType('float', $duration);
+
+    }
+
+    public function testFramesIsInt() {
+        $file = __DIR__.'/dummy/test.mp4';
+        $s = new Smoovie();
+        $s->make($file);
+
+        $duration = $s->frames();
+
+        $this->assertInternalType('int', $duration);
+
+    }
+
+    public function testFpsIsFloat()
+    {
+        $file = __DIR__.'/dummy/test.mp4';
+        $s = new Smoovie();
+        $s->make($file);
+
+        $duration = $s->fps();
+
+        $this->assertInternalType('float', $duration);
     }
 
 }
